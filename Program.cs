019bf22b2be166b556f49fa40c85ed036313b547
reqsanemustafayevaa <146015080+reqsanemustafayevaa@users.noreply.github.com>
2023-10-16 11:07:23 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace tuple_taskss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            (int sumEven, int Odd) tuple = Sum(arr);
+            Console.WriteLine(tuple.sumEven);
+            Console.WriteLine(tuple.Odd);
+        }
+        static(int,int) Sum(int[] arr)
+        {
+            int sumOdd = 0;
+            int sumEvenn = 0;
+            foreach (int num in arr)
+            {
+                if(num%2 == 0)
+                {
+                    sumEvenn += num;
+                }
+                else
+                {
+                    sumOdd += num;
+                }
+            }
+            return (sumEvenn, sumOdd);
+        }
+    }
+}
